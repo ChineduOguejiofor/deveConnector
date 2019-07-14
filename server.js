484da15793a64a4bf4c462759a2f2ad02489efcb
ChineduOguejiofor <chineduknight@gmail.com>
2019-07-14 21:50:07 +0100

    changed public

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ connectDB();
 //This is equivalent to bodyparser
 app.use(express.json({ extended: false }));
 
-app.use('/public', express.static('client/WorkwithUI/dist'));
-
 // app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
@@ -18,7 +16,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-
+app.use('/', express.static('client/WorkwithUI/dist'));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
